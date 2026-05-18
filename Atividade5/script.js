551fbd05594286c66153affa1afd61b77cbab16f
Gustavo <gustavo.deandrademoreira@gmail.com>
2026-05-18 19:37:58 -0300

    Atividade 5 (primeira com js)

--- Atividade5/script.js
+++ Atividade5/script.js
@@ -1,5 +1,24 @@
-let p = document.createElement("p")
+function check() {
+          let Trf = document.getElementById("Tarefa").value;
+          if (Trf === "") {
+               return
+          }
+          let prioridade = document.getElementById("Priorid").value;
+          let p = document.createElement("p")
+          let box = document.createElement("div")
+          box.classList.add("card", "d-inline-block")
+          p.innerHTML="Tarefa: "  + Trf + " | Prioridade: " + prioridade;
+          box.appendChild(p)
+          document.body.appendChild(box)
+          document.getElementById("container").appendChild(box);
 
-p.innerText="";
+          if (prioridade === "Alta") {
+                box.classList.add("bg-danger"); 
+                } 
 
-document.body.appendChild(p);
+          else {
+                box.classList.add("bg-success"); 
+                }
+
+               
+        }
